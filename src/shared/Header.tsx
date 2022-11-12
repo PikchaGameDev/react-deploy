6b77de50c39207c3f124207f77bbf0b7a0676aa7
PikchaGameDev <LawsAreBull@protonmail.com>
2022-11-13 02:39:25 +0300

    Added router

--- src/shared/Header.tsx
+++ src/shared/Header.tsx
@@ -1,12 +1,22 @@
 import { hot } from "react-hot-loader/root";
 import * as React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import styles from "./header.css";
 
 function HeaderComponent() {
   return (
-    <header>
-      <h1 className={styles.example}>Hello React</h1>
-    </header>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="*"
+          element={
+            <header>
+              <h1 className={styles.example}>Hello React</h1>
+            </header>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
