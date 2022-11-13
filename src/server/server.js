39833983787e3867836238204b9e885eb5a2319d
PikchaGameDev <LawsAreBull@protonmail.com>
2022-11-13 03:24:04 +0300

    fix

--- src/server/server.js
+++ src/server/server.js
@@ -2,15 +2,26 @@ import express from "express";
 import ReactDOM from "react-dom/server";
 import { Header } from "../shared/Header.tsx";
 import { indexTemplate } from "./indexTemplate";
+import cors from "cors";
+import compression from "compression";
+import helmet from "helmet";
+
 const app = express();
 
-const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(compression());
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false,
+  })
+);
 
 app.use("/static", express.static("./dist/client"));
 app.get("/", (req, res) => {
   res.send(indexTemplate(ReactDOM.renderToString(Header())));
 });
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("server started on port http://localhost:3000");
 });
